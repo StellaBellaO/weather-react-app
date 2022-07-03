846cc74e2d64b12120aaf0a16b7518fcdd0fb989
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2022-07-03 18:53:42 +0100

    Changed Icon to white

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
       return (
         <ReactAnimatedWeather
           icon={codeMapping[props.code]}
-          color="FFFFFF"
+          color="white"
           size={props.size}
           animate={true}
         />
