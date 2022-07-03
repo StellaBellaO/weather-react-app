49d384f2ac84094a1e2aac1ce4200761aaad7cd6
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2022-07-03 12:14:15 +0100

    amended font size footer

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function App() {
         Weather | <span className="font-weight-bold"> Forecast </span>{" "}
       </p>
       <Weather defaultCity="Lagos"/>
-      <footer className="open-sourced text-center mt-5"> 🗄️ This Project was coded by < a href="https://gleeful-bubblegum-ab91e6.netlify.app/index.html" target="_blank" rel="noopener noreferrer"> Stella-Maris Ojideh </a> {" "} and is {" "} <a href="https://github.com/StellaBellaO/weather-react-app" target="_blank" rel="noopener noreferrer" class="github">open-sourced on GitHub </a> {" "} and <a href="https://chimerical-manatee-089194.netlify.app/" target="_blank" rel="noopener noreferrer"> hosted om Netlify 🗄️ </a></footer>
+      <footer className="open-sourced text-center mt-5"> 🗄️ This Project was coded by < a href="https://gleeful-bubblegum-ab91e6.netlify.app/index.html" target="_blank" rel="noopener noreferrer"> Stella-Maris Ojideh </a> {" "} and is {" "} <a href="https://github.com/StellaBellaO/weather-react-app" target="_blank" rel="noopener noreferrer" class="github">open-sourced on GitHub </a> {" "} and <a href="https://chimerical-manatee-089194.netlify.app/" target="_blank" rel="noopener noreferrer"> hosted on Netlify 🗄️ </a></footer>
     </div>
   );
 }
